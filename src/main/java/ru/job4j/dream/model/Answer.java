6b5e6cdd04169c8cd35d934bd2d64bc766432af8
Stas Korobeynikov <stas.korobeinikov@mail.ru>
2020-07-09 13:59:00 +0300

    4.1. JSON формат.[#309432]

---
+++ src/main/java/ru/job4j/dream/model/Answer.java
@@ -0,0 +1,18 @@
+package ru.job4j.dream.model;
+
+public class Answer {
+
+    private String text;
+
+    public Answer(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
--- src/main/java/ru/job4j/dream/servlet/GreetingServlet.java
+++ src/main/java/ru/job4j/dream/servlet/GreetingServlet.java
@@ -1,21 +1,24 @@
 package ru.job4j.dream.servlet;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import ru.job4j.dream.model.Answer;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 public class GreetingServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("text/plain");
-        resp.setCharacterEncoding("UTF-8");
         String name = req.getParameter("name");
-        PrintWriter writer = new PrintWriter(resp.getOutputStream());
-        writer.println("Nice to meet you, " + name);
-        writer.flush();
+        ObjectMapper mapper = new ObjectMapper();
+        Answer answer = new Answer("Nice to meet you, " + name);
+        String jsonString = mapper.writeValueAsString(answer);
+        resp.setCharacterEncoding("UTF-8");
+        resp.setContentType("json");
+        resp.getWriter().write(jsonString);
     }
 }
