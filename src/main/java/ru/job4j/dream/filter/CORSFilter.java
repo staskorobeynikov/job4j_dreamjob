b404db60dde5b577a630436af34ec49b8c2b6380
Stas Korobeynikov <stas.korobeinikov@mail.ru>
2020-12-26 20:05:19 +0300

    Add CORS Filter[#309434]

--- src/main/java/ru/job4j/dream/filter/CORSFilter.java
+++ src/main/java/ru/job4j/dream/filter/CORSFilter.java
@@ -11,7 +11,7 @@ public class CORSFilter implements Filter {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
 
         ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Origin", "*");
-        ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Methods","GET, OPTIONS, HEAD, PUT, POST");
+        ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Methods", "GET, OPTIONS, HEAD, PUT, POST");
 
         HttpServletResponse resp = (HttpServletResponse) servletResponse;
 
